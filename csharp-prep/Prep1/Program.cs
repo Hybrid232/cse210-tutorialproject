cc559191ac16946efbbd28e300f3e8ef285be401
EmmittScott <hybrid23@byui.edu>
2025-01-11 12:13:26 -0700

    Completed Assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //Using Console.WriteLine will make the response enter, V.S. Console.Write keeps it on the same line//
+        Console.Write("What is your first name: ");
+
+        //let's user input their response then records it in the firstName variable//
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your last name: ");
+        string lastName = Console.ReadLine();
+
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
