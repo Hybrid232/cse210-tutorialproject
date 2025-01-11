00e27cd04686278bb82fcb5cf0e3bc4db034831f
EmmittScott <hybrid23@byui.edu>
2025-01-11 13:14:05 -0700

    Completed Assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade: ");
+        string gradeValue = Console.ReadLine();
+        int grade = int.Parse(gradeValue);
+
+        string gradeLetter = "";
+       
+
+        if (grade >= 90)
+        {
+            gradeLetter = "A";
+            
+        }
+        else if (grade >= 80)
+        {
+            gradeLetter = "B";
+        }
+        else if (grade >= 70)
+        {
+            gradeLetter = "C";
+        }
+        else if (grade >= 60)
+        {
+            gradeLetter = "D";
+        }
+        else
+        {
+            gradeLetter = "F";
+        }
+
+        Console.WriteLine($"You got a {gradeLetter} in this class!");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("And you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Darn, better luck next time!");
+        }
     }
 }
