4a1f5eb4fce109ed29de4e7aaadfd234abbcb24f
EmmittScott <hybrid23@byui.edu>
2025-01-15 11:18:56 -0700

    Completed Assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+         
+
+        
+        Random ranNum = new Random();
+        int numSecret = ranNum.Next(1, 15);
+        int playerInput = 0;
+
+        while (playerInput != numSecret)
+        {
+            Console.Write("What is the Secret Number? ");
+            
+            playerInput = int.Parse(Console.ReadLine());
+            
+
+            if (playerInput != numSecret)
+            {
+                Console.WriteLine("Not Right, Try again");
+            }
+
+            if (playerInput > numSecret)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else if (playerInput < numSecret)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else if (playerInput == numSecret)
+            {
+                Console.Write("That's the Secret Number!");
+                break;
+            }
+        }
+
+
     }
 }
