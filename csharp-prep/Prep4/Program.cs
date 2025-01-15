8c4b6eaca222617fdf44662c9f0f564e6489d30f
EmmittScott <hybrid23@byui.edu>
2025-01-15 13:16:57 -0700

    Completed Assignment

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Linq;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> inputNumbers = new List<int>();
+        Console.WriteLine("Make a list of as many numbers as you want! Press 0 when finished.");
+        int userInput = -1;
+
+        while (userInput != 0)
+        {
+            Console.Write("Input Number: ");
+            userInput = int.Parse(Console.ReadLine());
+            inputNumbers.Add(userInput);
+            
+
+            if (userInput == 0)
+            {
+                int listSum = inputNumbers.AsQueryable().Sum();
+                double listAverage = Queryable.Average(inputNumbers.AsQueryable());
+                int listMax = inputNumbers.Max();
+                
+                Console.WriteLine($"Sum: {listSum}");
+                Console.WriteLine($"Average: {listAverage}");
+                Console.WriteLine($"Highest Number: {listMax}");
+            }
+        }
+        
+
+
     }
 }
