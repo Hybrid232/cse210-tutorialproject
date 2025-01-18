40171588b8be024fb5c5af6a33f45aba4d25fed5
EmmittScott <hybrid23@byui.edu>
2025-01-18 13:56:31 -0700

    Completed Assignment

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int favNumber = PromptUserNumber();
+        int sqrNum = SquareNumber(favNumber);
+        
+        
+        DisplayResult(userName, sqrNum);
+        
+    }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Enter your name: ");
+        string input = Console.ReadLine();
+
+        return input;
+    }
+
+
+    static int PromptUserNumber()
+    {
+        Console.Write($"What is your favorite number: ");
+        int favNumber = int.Parse(Console.ReadLine());
+
+        return favNumber;
+    }
+    
+    static int SquareNumber(int userNumber)
+    {
+        int sqr = userNumber * userNumber;
+
+        return sqr;
+    }
+
+    static void DisplayResult(string input, int sqr)
+    {
+        Console.WriteLine($"{input}, the square root of your number is {sqr}!");
+
+
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!!!! :)");
+        /*
+        The names of the variable
+        GOod stuff
+        */
+
+        string adjName = GetAdjective();
+        string nounName = GetNoun();
+
+        int number = Multiply(3, 4);
+
+        Console.WriteLine($"I looked out the window and saw {number} {adjName} {nounName}s!");
     }
+
+        static int Multiply(int number1, int number2)
+        {
+            int product = number1 * number2;
+
+            return product;
+        }
+        static string GetAdjective()
+        {
+            List<string> words = new List<string>();
+            words.Add("Blue");
+            words.Add("Yellow");
+            words.Add("Big");
+            words.Add("Small");
+
+            string adjName = words[2];
+
+            return adjName;    
+        }
+
+        static string GetNoun()
+        {
+            string nounName = "bird";
+
+            return nounName;
+        }
 }
