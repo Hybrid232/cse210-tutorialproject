18a8e23f3423ee40c04c79280af5e43504b0ffaf
EmmittScott <hybrid23@byui.edu>
2025-01-18 14:24:03 -0700

    New Code for Completed Assignment

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,11 +1,65 @@
 using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
+
+        List<int> inputNumbers = new List<int>();
+        Console.WriteLine("Make a list of as many numbers as you want! Press 0 when finished.");
+        int userInput = -1;
+
+        while (userInput != 0)
+        {
+            Console.Write("Input Number: ");
+            userInput = int.Parse(Console.ReadLine());
+            inputNumbers.Add(userInput);
+        }
+
+
+        int listSum = 0;
+        foreach (int value in inputNumbers)
+        {
+            listSum += value;
+        }
+        Console.WriteLine($"The Sum of your numbers is {listSum}");
+
+        float listAverage = ((float)listSum) / inputNumbers.Count;
+        Console.WriteLine($"The Average of your list is {listAverage}");
+
+
+        int listMax = inputNumbers[0];
+
+        foreach (int value in inputNumbers)
+        {
+            if (value > listMax)
+            {
+                listMax = value;
+            }
+
+        }
+        Console.WriteLine($"The Max of your list is {listMax}");
+    }   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /* OLD CODE
         List<int> inputNumbers = new List<int>();
         Console.WriteLine("Make a list of as many numbers as you want! Press 0 when finished.");
         int userInput = -1;
@@ -28,8 +82,7 @@ class Program
                 Console.WriteLine($"Highest Number: {listMax}");
             }
         }
-        
+        */
 
 
-    }
-}
+    
