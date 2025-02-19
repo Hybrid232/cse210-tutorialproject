7a5701a7c52da7739e0ef81270039b00040a9a23
EmmittScott <hybrid23@byui.edu>
2025-02-19 12:05:22 -0700

    Assignment Completed

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Mark Stevenson", "Addition");
+        Console.WriteLine(assignment1.GetSummary());
+
+        Console.WriteLine("");
+
+        MathAssignment assignment2 = new MathAssignment("Jerome Stevenson", "Subtraction", "5.5", "15-20");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+
+        Console.WriteLine("");
+
+        WritingAssignment assignment3 = new WritingAssignment("Iris Stevenson", "Religious History", "Writings of Ancient Prophets");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInfo());
+        
     }
 }
---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,20 @@
+public class WritingAssignment : Assignment
+{
+    private string _title;
+
+
+
+    public WritingAssignment(string studentName, string topic, string bookTitle) : base(studentName, topic)
+    {
+        _title = bookTitle;
+    }
+    
+    public string GetWritingInfo()
+    {
+        string studentName = GetStudentName();
+
+        return $"{_title} by {studentName}";
+    }
+
+
+}
