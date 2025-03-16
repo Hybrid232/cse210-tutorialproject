d807c700d3326f3b9c23e004a39d87483473be7b
EmmittScott <hybrid23@byui.edu>
2025-03-15 23:42:17 -0600

    Project Update

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -1,9 +1,55 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
+    static void MainMenu()
+    {
+        Console.WriteLine("1. Catalogue");
+        Console.WriteLine("2. View Shopping Cart");
+        Console.WriteLine("3. Personal Information");
+        Console.WriteLine("4. Leave Store");
+        Console.WriteLine("");
+        Console.Write("Select: ");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello FinalProject World!");
+        bool repeat = true;
+
+        Console.WriteLine("Welcome to ManyItemsMart!");
+        Console.WriteLine("Great to have you here today!");
+
+        while(repeat)
+        {
+            MainMenu();
+            string userInput = Console.ReadLine();
+
+            if (userInput == "1")
+            {
+                Product productSelect = new Product();
+                productSelect.GetProduce();
+                Console.ReadLine();
+            }
+            else if (userInput == "2")
+            {
+
+            }
+            else if (userInput == "3")
+            {
+
+            }
+            else if (userInput == "4")
+            {
+                repeat = false;
+
+            }
+            else
+            {
+
+            }
+
+
+        }
+        
     }
 }
